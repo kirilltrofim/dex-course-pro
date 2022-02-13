f5d840a2b9fe0ccb78c05f06725bcbd24aebc19f
kirilltrofim_PC <kirill.trofim@gmail.com>
2022-02-13 17:36:41 +0200

    add independentWork14

---
+++ independentWork14/src/App.tsx
@@ -0,0 +1,12 @@
+import "./styles.scss";
+import { TransitionComponent } from "./TransitionComponent";
+import { BackFon } from "./BackFon";
+
+export default function App() {
+  return (
+    <div className="App">
+      <BackFon />
+      <TransitionComponent />
+    </div>
+  );
+}
---
+++ independentWork14/src/TransitionComponent.tsx
@@ -0,0 +1,14 @@
+export const TransitionComponent = () => {
+  return (
+    <>
+      <div className="">
+        <input className={"check"} type={"checkbox"} />
+        <div> </div>
+        <div> </div>
+        <div> </div>
+        <div> </div>
+        <p className="magicText">Магия вне хогвартса</p>
+      </div>
+    </>
+  );
+};
